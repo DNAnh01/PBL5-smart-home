9f36e449fca515d4d71e3e131175722efd19e6b7
DNAnh01 <donguyenanhgithub@gmail.com>
2023-03-03 03:39:38 +0700

    CNN padding stride

--- CNN/convolution.py
+++ CNN/convolution.py
@@ -2,46 +2,75 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('./CNN/imagesData/face.jpg')
-# chuyển kích thức img thành khung hình vuông
-img = cv2.resize(img, (1200, 1200))
-# chuyển màu của img thành màu xám
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# print(img_gray.shape) # (1200, 1200)
+# np.random.seed(5) # The random times will produce the same number
 
+img = cv2.imread('./CNN/imagesData/face.jpg')
+img = cv2.resize(img, (1200, 1200))
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)/255 # Convert to gray and normalization
+# print(img_gray.shape) # (1200, 1200)
+print(img_gray)
 # convolution
 
 class Conv2d:
-    def __init__(self, input, kernelSize):
-        self.input = input
-        # kích cỡ của img
-        self.height, self.width = input.shape
+    def __init__(self, input, kernelSize, padding=0, stride=1):
+        self.input = np.pad(input, ((padding, padding), (padding, padding)), 'constant') # Conv's own input
+        self.stride = stride 
         self.kernel = np.random.randn(kernelSize, kernelSize)
-        # print(kernel)
+        # print(self.kernel)
         '''
-        [[ 0.23512125 -0.68516903 -0.5429533 ]
-        [-0.00981667 -0.42392471  1.04781965]
-        [-1.27972119 -0.23502826  1.29476504]]
+        [[ 0.44122749 -0.33087015  2.43077119]
+        [-0.25209213  0.10960984  1.58248112]
+        [-0.9092324  -0.59163666  0.18760323]]
         '''
-        self.results = np.zeros((self.height - kernelSize + 1, self.width - kernelSize + 1))
-        # roi -> region of interesting 
+        self.results = np.zeros((int((self.input.shape[0] - self.kernel.shape[0])/self.stride) + 1,
+                                 int((self.input.shape[1] - self.kernel.shape[1])/self.stride) + 1))
+
     def getROI(self):
         """
-        tìm vùng dữ liệu quan tâm
+        Region Of Interesting
         """
-        for row in range(0, self.height - self.kernel.shape[0] + 1):
-            for column in range(0, self.width - self.kernel.shape[1] + 1):
-                roi = self.input[row: row + self.kernel.shape[0], column: column + self.kernel.shape[1]]
+        for row in range(int((self.input.shape[0] - self.kernel.shape[0])/self.stride) + 1):
+            for column in range(int((self.input.shape[1] - self.kernel.shape[1])/self.stride) + 1):
+                roi = self.input[row*self.stride: row*self.stride + self.kernel.shape[0],
+                                 column*self.stride: column*self.stride + self.kernel.shape[1]]
                 yield row, column, roi
 
     def operate(self):
         for row, column, roi in self.getROI():
             self.results[row, column] = np.sum(roi * self.kernel)
-
         return self.results
 
-conv2d = Conv2d(img_gray, 5)
+class ReLU:
+    def __init__(self, input):
+        self.input = input
+        self.result = np.zeros((self.input.shape[0], self.input.shape[1]))
 
-img_gray_conv2d = conv2d.operate()
-plt.imshow(img_gray_conv2d, cmap='gray')
+    def operate(self):
+        for row in range(self.input.shape[0]):
+            for column in range(self.input.shape[1]):
+                self.result[row, column] = 0 if self.input[row, column] < 0 else self.input[row, column]
+        return self.result
+    
+class LeakyReLU:
+    def __init__(self, input):
+        self.input = input
+        self.result = np.zeros((self.input.shape[0], self.input.shape[1]))
+
+    def operate(self):
+        for row in range(self.input.shape[0]):
+            for column in range(self.input.shape[1]):
+                self.result[row, column] = 0.1*self.input[row, column] if self.input[row, column] < 0 else self.input[row, column]
+        return self.result
+
+fig = plt.figure(figsize=(12, 12))
+for i in range(1, 10):
+    conv2d = Conv2d(input=img_gray,kernelSize= 3, padding=2, stride=i)
+    img_gray_conv2d = conv2d.operate()
+    conv2d_relu = ReLU(input=img_gray_conv2d)
+    img_gray_conv2d_relu = conv2d_relu.operate()
+
+    plt.subplot(3, 3, i)
+    plt.imshow(img_gray_conv2d_relu, cmap='gray')
+    plt.axis('off')
+
 plt.show()
--- README.md
+++ README.md
@@ -1,25 +1,58 @@
 # Convolutional Neural Network (CNN)
 
-<img src="/imagesMarkdown/Screenshot 2023-03-02 024303.png" width="600" height="360">
+<img src="/imagesMarkdown/Screenshot 2023-03-02 024303.png" width="" height="">
 
 ## Convolution
 
 
-<img src="/imagesMarkdown/conv-full-layer.gif" width="543" height="544">
+<img src="/imagesMarkdown/conv-full-layer.gif" width="" height="">
 
 
 *   Ma trận bên trái nhất tượng trưng cho tấm hình của mình, mỗi một ô là 1 pixel
 
 *   Ma trận hình vuông ở giữa (3*3) gọi là kernel và nó chỉ có kích thước lẻ ví dụ như 3\*3, 5\*5, 7\*7, ..không bao giờ lấy số chẵn.
 
-*   Ma trận bên trái là ma trận kết quả sau khi lấy mt bên trái chập với ma trận 3\*3 ở giữa
+*   Ma trận bên trái là ma trận kết quả sau khi lấy mt bên trái chập với ma trận 3\*3 ở giữa.
 
 
-<img src="/imagesMarkdown/Screenshot 2023-03-02 042049.png" width="600" height="360">
+<img src="/imagesMarkdown/Screenshot 2023-03-02 042049.png" width="" height="">
 
 ## CNN no padding 
 
-<img src="/imagesMarkdown/Convolution_arithmetic_-_No_padding_strides.gif" width="600" height="500">
+<img src="/imagesMarkdown/Convolution_arithmetic_-_No_padding_strides.gif" width="" height="">
 
+## Stride
 
+*   Mỗi lần chập thì kernel sẽ di chuyển với bước nhảy = stride.
 
+*   Stride càng lớn có thể làm thông tin bị mất
+
+<img src="/imagesMarkdown/Stride_convolution.gif" width="" height="">
+
+## CNN with padding 
+
+*   Các pixel ở viền sẽ được tính toán vài lần so với chỉ được tính toán một lần khi no padding.
+
+<img src="/imagesMarkdown/Convolution_arithmetic_-_Padding_strides.gif" width="" height="">
+
+## ReLU (Rectified Linear Unit)
+
+<img src="/imagesMarkdown/ReLU-activation-function.png" width="" height="">
+
+*   ReLU (Rectified Linear Unit) là một hàm kích hoạt được sử dụng trong các lớp convolutional neural network (CNN) để giảm thiểu hiện tượng vanishing gradient (mất mát độ dốc) và tăng tính phi tuyến tính của mô hình.
+
+*   Hàm ReLU có công thức đơn giản: f(x) = max(0,x), trong đó x là giá trị đầu vào và max(0,x) trả về giá trị của x nếu x lớn hơn hoặc bằng 0, ngược lại trả về 0.
+
+*   ReLU được sử dụng như một hàm kích hoạt sau các lớp convolutional và pooling để thực hiện một số chức năng quan trọng trong mạng CNN, bao gồm:
+
+    1.  Tính phi tuyến tính: ReLU giúp giảm thiểu tính tuyến tính của mô hình bằng cách đưa vào không gian phi tuyến tính cho đầu ra.
+
+    2.  Loại bỏ giá trị âm: ReLU loại bỏ giá trị âm và giữ lại giá trị không âm, giúp tăng tốc độ tính toán và giảm sự phụ thuộc vào các tham số.
+
+    3.  Không độc quyền (non-exclusive): ReLU là một hàm không độc quyền, nghĩa là nhiều giá trị đầu vào có thể trả về cùng một giá trị đầu ra. Điều này giúp giảm thiểu hiện tượng overfitting (quá khớp) trong mô hình.
+
+*   Với những lợi ích trên, ReLU đã trở thành một lựa chọn phổ biến để sử dụng trong các mạng CNN.
+
+---
+
+<img src="/imagesMarkdown/Screenshot 2023-03-03 033013.png" width="" height="">
