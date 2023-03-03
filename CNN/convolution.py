82acc1e472bef4aaa631f6c299a0a86493f0a395
DNAnh01 <donguyenanhgithub@gmail.com>
2023-03-03 22:21:01 +0700

    tăng layer kernel và code hàm MaxPooling

--- CNN/convolution.py
+++ CNN/convolution.py
@@ -2,75 +2,138 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# np.random.seed(5) # The random times will produce the same number
+np.random.seed(5)  # The random times will produce the same number
 
 img = cv2.imread('./CNN/imagesData/face.jpg')
 img = cv2.resize(img, (1200, 1200))
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)/255 # Convert to gray and normalization
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)/255  # Convert to gray and normalization
 # print(img_gray.shape) # (1200, 1200)
-print(img_gray)
-# convolution
+# print(img_gray)
+# print(img.shape) # (1200, 1200, 3)
+'''
+    Đối vs img thì parameter số lượng layer nằm ở đằng sau
+    Còn đối vs np.array thì parameter số lượng layer nằm ở đằng trc
+'''
+# Convolution
+
 
 class Conv2d:
-    def __init__(self, input, kernelSize, padding=0, stride=1):
-        self.input = np.pad(input, ((padding, padding), (padding, padding)), 'constant') # Conv's own input
-        self.stride = stride 
-        self.kernel = np.random.randn(kernelSize, kernelSize)
+    def __init__(self, input, numberOfKernel=8, kernelSize=3, padding=0, stride=1):
+        # Conv's own input
+        self.input = np.pad(input, ((padding, padding), (padding, padding)), 'constant')
+        self.stride = stride
+        '''
+            self.kernel  bản chất như np.array thì parameter số lượng layer nằm ở đằng trc
+        '''
+        self.kernel = np.random.randn(numberOfKernel, kernelSize, kernelSize)
         # print(self.kernel)
         '''
         [[ 0.44122749 -0.33087015  2.43077119]
         [-0.25209213  0.10960984  1.58248112]
         [-0.9092324  -0.59163666  0.18760323]]
         '''
-        self.results = np.zeros((int((self.input.shape[0] - self.kernel.shape[0])/self.stride) + 1,
-                                 int((self.input.shape[1] - self.kernel.shape[1])/self.stride) + 1))
+        # print(self.kernel.shape) # (8, 3, 3)
+
+        '''
+            self.results bản chất cũng là những chỉ số dữ liệu của tấm hình nên parameter số lượng 
+            layer phải nằm ở đằng sau
+        '''
+        self.results = np.zeros((int((self.input.shape[0] - self.kernel.shape[1])/self.stride) + 1,
+                                 int((self.input.shape[1] - self.kernel.shape[2])/self.stride) + 1,
+                                 self.kernel.shape[0]))
 
     def getROI(self):
-        """
-        Region Of Interesting
-        """
-        for row in range(int((self.input.shape[0] - self.kernel.shape[0])/self.stride) + 1):
-            for column in range(int((self.input.shape[1] - self.kernel.shape[1])/self.stride) + 1):
-                roi = self.input[row*self.stride: row*self.stride + self.kernel.shape[0],
-                                 column*self.stride: column*self.stride + self.kernel.shape[1]]
+        '''
+            Region Of Interesting
+            Hàm này dựa trên tấm hình và chọn ra vùng để tính toán
+        '''
+        for row in range(int((self.input.shape[0] - self.kernel.shape[1])/self.stride) + 1):
+            for column in range(int((self.input.shape[1] - self.kernel.shape[2])/self.stride) + 1):
+                roi = self.input[row*self.stride: row*self.stride + self.kernel.shape[1],
+                                 column*self.stride: column*self.stride + self.kernel.shape[2]]
                 yield row, column, roi
 
     def operate(self):
-        for row, column, roi in self.getROI():
-            self.results[row, column] = np.sum(roi * self.kernel)
+        '''
+            Tính toán trên nhiều layer
+        '''
+        for layer in range(self.kernel.shape[0]):
+            for row, column, roi in self.getROI():
+                self.results[row, column, layer] = np.sum(roi * self.kernel[layer, :, :])
         return self.results
+
 
 class ReLU:
     def __init__(self, input):
+        '''
+        input = result of convolution
+        '''
         self.input = input
-        self.result = np.zeros((self.input.shape[0], self.input.shape[1]))
+        self.result = np.zeros((self.input.shape[0],
+                                self.input.shape[1],
+                                self.input.shape[2]))
 
     def operate(self):
-        for row in range(self.input.shape[0]):
-            for column in range(self.input.shape[1]):
-                self.result[row, column] = 0 if self.input[row, column] < 0 else self.input[row, column]
+        for layer in range(self.input.shape[2]):
+            for row in range(self.input.shape[0]):
+                for column in range(self.input.shape[1]):
+                    self.result[row, column, layer] = 0 if self.input[row, column, layer] < 0 else self.input[row, column, layer]
         return self.result
-    
+
+
 class LeakyReLU:
     def __init__(self, input):
         self.input = input
-        self.result = np.zeros((self.input.shape[0], self.input.shape[1]))
+        self.result = np.zeros((self.input.shape[0],
+                                self.input.shape[1],
+                                self.input.shape[2]))
 
     def operate(self):
-        for row in range(self.input.shape[0]):
-            for column in range(self.input.shape[1]):
-                self.result[row, column] = 0.1*self.input[row, column] if self.input[row, column] < 0 else self.input[row, column]
+        for layer in range(self.input.shape[2]):
+            for row in range(self.input.shape[0]):
+                for column in range(self.input.shape[1]):
+                    self.result[row, column, layer] = 0.1*self.input[row, column, layer] if self.input[row, column, layer] < 0 else self.input[row, column, layer]
         return self.result
 
-fig = plt.figure(figsize=(12, 12))
-for i in range(1, 10):
-    conv2d = Conv2d(input=img_gray,kernelSize= 3, padding=2, stride=i)
-    img_gray_conv2d = conv2d.operate()
-    conv2d_relu = ReLU(input=img_gray_conv2d)
-    img_gray_conv2d_relu = conv2d_relu.operate()
 
-    plt.subplot(3, 3, i)
-    plt.imshow(img_gray_conv2d_relu, cmap='gray')
-    plt.axis('off')
+class MaxPooling:
+    def __init__(self, input, poolingSize=2):
+        self.input = input
+        self.poolingSize = poolingSize
+        self.result = np.zeros((int(self.input.shape[0]/self.poolingSize),
+                                int(self.input.shape[1]/self.poolingSize),
+                                self.input.shape[2]))
 
-plt.show()
+    def operate(self):
+        for layer in range(self.input.shape[2]):
+            for row in range(int(self.input.shape[0]/self.poolingSize)):
+                for column in range(int(self.input.shape[1]/self.poolingSize)):
+                    self.result[row, column, layer] = np.max(self.input[row*self.poolingSize: row*self.poolingSize + self.poolingSize,
+                                                                 column*self.poolingSize: column*self.poolingSize + self.poolingSize,
+                                                                 layer])
+        return self.result
+
+# img_gray_conv2d = Conv2d(input=img_gray,numberOfKernel=16,kernelSize= 3, padding=0, stride=2).operate()
+# img_gray_conv2d_relu = ReLU(img_gray_conv2d).operate()
+# img_gray_conv2d_leakyrelu = LeakyReLU(img_gray_conv2d).operate()
+# img_gray_conv2d_leakyrelu_maxpooling = MaxPooling(img_gray_conv2d_leakyrelu, poolingSize=3).operate()
+# fig = plt.figure(figsize=(8, 8))
+# for i in range(16):
+#     plt.subplot(4, 4, i+1)
+#     plt.imshow(img_gray_conv2d_leakyrelu_maxpooling[:, :, i], cmap='gray')
+#     plt.axis('off')
+# plt.savefig('img_gray_conv2d_leakyrelu_maxpooling.jpg')
+# plt.show()
+
+# fig = plt.figure(figsize=(12, 12))
+# for i in range(1, 10):
+#     conv2d = Conv2d(input=img_gray,kernelSize= 3, padding=0, stride=2)
+#     img_gray_conv2d = conv2d.operate()
+#     conv2d_relu = ReLU(input=img_gray_conv2d)
+#     img_gray_conv2d_relu = conv2d_relu.operate()
+
+#     plt.subplot(3, 3, i)
+#     plt.imshow(img_gray_conv2d_relu, cmap='gray')
+#     plt.axis('off')
+
+# plt.show()
--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 ## Convolution
 
-
 <img src="/imagesMarkdown/conv-full-layer.gif" width="" height="">
 
 
@@ -37,8 +36,6 @@
 
 ## ReLU (Rectified Linear Unit)
 
-<img src="/imagesMarkdown/ReLU-activation-function.png" width="" height="">
-
 *   ReLU (Rectified Linear Unit) là một hàm kích hoạt được sử dụng trong các lớp convolutional neural network (CNN) để giảm thiểu hiện tượng vanishing gradient (mất mát độ dốc) và tăng tính phi tuyến tính của mô hình.
 
 *   Hàm ReLU có công thức đơn giản: f(x) = max(0,x), trong đó x là giá trị đầu vào và max(0,x) trả về giá trị của x nếu x lớn hơn hoặc bằng 0, ngược lại trả về 0.
@@ -53,6 +50,48 @@
 
 *   Với những lợi ích trên, ReLU đã trở thành một lựa chọn phổ biến để sử dụng trong các mạng CNN.
 
----
+<img src="/imagesMarkdown/ReLU-activation-function.png" width="" height="">
 
-<img src="/imagesMarkdown/Screenshot 2023-03-03 033013.png" width="" height="">
+## Leaky ReLU
+
+*   Leaky ReLU là một hàm kích hoạt được sử dụng trong các mô hình mạng neural, bao gồm các mô hình Convolutional Neural Network (CNN). Hàm Leaky ReLU tương tự như Rectified Linear Unit (ReLU) với hàm kích hoạt phi tuyến tính, nhưng thay vì có giá trị 0 khi đầu vào âm, nó sẽ trả về giá trị nhỏ hơn 0 một lượng nhỏ được xác định trước đó.
+
+*   Cụ thể, hàm Leaky ReLU được định nghĩa bởi công thức:
+
+    f(x) = x nếu x > 0
+
+    f(x) = ax nếu x <= 0 (với a là một hằng số nhỏ dương)
+
+*   Trong các mô hình CNN, hàm Leaky ReLU thường được sử dụng thay cho ReLU vì nó giúp tránh hiện tượng "neuron chết" (dead neurons), khi một số neuron không hoạt động do có giá trị đầu vào âm quá lớn và đưa về giá trị 0. Bằng cách cho phép một lượng nhỏ giá trị âm đi qua, hàm Leaky ReLU giúp duy trì tính phi tuyến tính của mô hình và tăng tính linh hoạt trong việc học các đặc trưng của ảnh.
+
+<img src="/imagesMarkdown/LeakyReLU-activation-function.png" width="" height="">
+
+---
+### *img gray conv2d*
+
+<img src="/img_gray_conv2d.jpg" width="" height="">
+
+### *img gray conv2d relu*
+
+<img src="/img_gray_conv2d_relu.jpg" width="" height="">
+
+### *img gray conv2d leaky relu*
+
+<img src="/img_gray_conv2d_leakyrelu.jpg" width="" height="">
+
+## Max Pooling 
+
+*   Max Pooling là một kỹ thuật được sử dụng trong Convolutional Neural Network (CNN) để giảm kích thước của đầu vào thông qua việc lấy giá trị lớn nhất (max) của mỗi vùng đặc trưng (feature map) được chia thành các ô nhỏ.
+
+*   Cụ thể, trong quá trình max pooling, ta chia vùng đặc trưng thành các ô nhỏ, và lấy giá trị lớn nhất trong mỗi ô để tạo thành một feature map mới có kích thước nhỏ hơn. Quá trình này giúp giảm số lượng tham số của mô hình, đồng thời giúp giảm khả năng overfitting và làm giảm độ phức tạp của mô hình.
+
+*   Điều quan trọng cần lưu ý là kích thước và bước nhảy (stride) của max pooling cần phải được chọn sao cho feature map được giảm kích thước một cách hợp lý, nhưng vẫn giữ được độ chi tiết của thông tin trong ảnh. Ví dụ, nếu ta chọn kích thước và stride quá lớn, ta sẽ mất mát quá nhiều thông tin trong quá trình max pooling, dẫn đến việc giảm độ chính xác của mô hình.
+
+*   Max pooling thường được sử dụng sau các lớp Convolution và ReLU để giảm kích thước của feature map và giúp cho mô hình trở nên nhẹ hơn và dễ dàng xử lý hơn.
+
+<img src="/imagesMarkdown/MaxpoolSample2.png" width="" height="">
+
+### *img gray conv2d leaky relu maxpooling*
+
+<img src="/img_gray_conv2d_leakyrelu_maxpooling.jpg" width="" height="">
+
