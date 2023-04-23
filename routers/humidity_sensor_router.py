cb8a3becc1a280da5612d54de618fdff8c552f2b
DNAnh01 <donguyenanhgithub@gmail.com>
2023-04-24 04:06:16 +0700

    fix router :v

--- routers/humidity_sensor_router.py
+++ routers/humidity_sensor_router.py
@@ -18,7 +18,7 @@ async def get_humidity_sensor() -> HumiditySensor:
         raise HTTPException(status_code=404, detail="humidity sensor not found.")
     return result
 
-@router.put("temperature_sensor/update", response_model=HumiditySensor, tags=["humidity sensor"])
+@router.put("humidity_sensor/update", response_model=HumiditySensor, tags=["humidity sensor"])
 async def update_humidity_sensor(humidity_sensor_update: HumiditySensor = Body(...)) -> HumiditySensor:
     return humidity_sensor_service.update_humidity_sensor(humidity_sensor_update)
 
