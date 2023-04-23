4f47b96815a13f61fdf614784c0341282cd52778
DNAnh01 <donguyenanhgithub@gmail.com>
2023-04-24 03:55:08 +0700

    fix put router

--- schemas/temperature_sensor_schema.py
+++ schemas/temperature_sensor_schema.py
@@ -3,12 +3,12 @@ from datetime import datetime
 
 class TemperatureSensor(BaseModel):
     timestamp: datetime = Field(..., description="Timestamp of the image capture in ISO 8601 format")
-    data: float
+    data: int
     
     class Config:
         schema_extra = {
             "example": {
                 "timestamp": "2023-04-09T12:34:56.789Z",
-                "data": 32.5
+                "data": 32
             }
         }
