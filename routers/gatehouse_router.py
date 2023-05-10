08a7f71ca4c3d4536f008c6a2c80b355177fbbdc
DNAnh01 <donguyenanhgithub@gmail.com>
2023-05-10 21:53:56 +0700

    ws3

--- routers/gatehouse_router.py
+++ routers/gatehouse_router.py
@@ -34,7 +34,7 @@ gatehouse_service = GatehouseService()
 router = APIRouter()
 connected_clients: Dict[str, WebSocket] = {}  # Sử dụng dict để lưu trữ các client kết nối
 
-@router.put("/gatehouse/update/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
+@router.put("/gatehouse/home/update/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
 async def update_gatehouse(websocket: WebSocket, gatehouse_document_ID: str, gatehouse_update: GateHouse = Body(...)) -> GateHouse:
     result = gatehouse_service.get_gatehouse(gatehouse_document_ID)
     if not result:
@@ -45,6 +45,23 @@ async def update_gatehouse(websocket: WebSocket, gatehouse_document_ID: str, gat
 
     # Gửi dữ liệu cập nhật cho client app Android có địa chỉ IP cụ thể
     target_ip = "27.69.146.73"  # Địa chỉ IP cần gửi dữ liệu
+    if target_ip in connected_clients:
+        target_client = connected_clients[target_ip]
+        await target_client.send_json(gatehouse_update.dict())
+
+    return gatehouse_service.update_gatehouse(gatehouse_document_ID, gatehouse_update)
+
+@router.put("/gatehouse/app/update/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
+async def update_gatehouse(websocket: WebSocket, gatehouse_document_ID: str, gatehouse_update: GateHouse = Body(...)) -> GateHouse:
+    result = gatehouse_service.get_gatehouse(gatehouse_document_ID)
+    if not result:
+        raise HTTPException(status_code=404, detail="gatehouse not found.")
+
+    # Lưu trữ client WebSocket theo địa chỉ IP
+    connected_clients[websocket.client.host] = websocket
+
+    # Gửi dữ liệu cập nhật cho client app Android có địa chỉ IP cụ thể
+    target_ip = "192.168.1.8"  # Địa chỉ IP cần gửi dữ liệu
     if target_ip in connected_clients:
         target_client = connected_clients[target_ip]
         await target_client.send_json(gatehouse_update.dict())
