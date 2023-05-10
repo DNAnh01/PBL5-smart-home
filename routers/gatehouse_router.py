7338f5e2ed7b49018a1e1ee8be4cdd1de32934b1
DNAnh01 <donguyenanhgithub@gmail.com>
2023-05-10 21:23:59 +0700

    ws

--- routers/gatehouse_router.py
+++ routers/gatehouse_router.py
@@ -1,24 +1,52 @@
-from fastapi import APIRouter, Body, HTTPException
-from services.gatehouse_service import GatehouseService
-from schemas.gatehouse_schema import GateHouse
+# from fastapi import APIRouter, Body, HTTPException, WebSocket
+# from services.gatehouse_service import GatehouseService
+# from schemas.gatehouse_schema import GateHouse
 
-router = APIRouter()
+# router = APIRouter()
+# gatehouse_service = GatehouseService()
+
+# @router.post("/gatehouse/create", response_model=GateHouse, tags=["gatehouse"])
+# async def create_gatehouse(gatehouse_create: GateHouse = Body(...)) -> GateHouse:
+#     return gatehouse_service.create_gatehouse(gatehouse_create)
+
+# @router.get("/gatehouse/get/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
+# async def get_gatehouse(gatehouse_document_ID: str) -> GateHouse:
+#     result = gatehouse_service.get_gatehouse(gatehouse_document_ID)
+#     if not result:
+#         raise HTTPException(status_code=404, detail="gatehouse not found.")
+#     return result
+
+# @router.put("/gatehouse/update/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
+# async def update_gatehouse(websocket: WebSocket, gatehouse_document_ID: str, gatehouse_update: GateHouse = Body(...)) -> GateHouse:
+#     result = gatehouse_service.get_gatehouse(gatehouse_document_ID)
+#     if not result:
+#         raise HTTPException(status_code=404, detail="gatehouse not found.")
+#     return gatehouse_service.update_gatehouse(gatehouse_document_ID, gatehouse_update)
+
+from fastapi import APIRouter, Body, HTTPException
+from fastapi import WebSocket
+from fastapi.responses import JSONResponse
+from schemas.gatehouse_schema import GateHouse
+from typing import Dict
+from services.gatehouse_service import GatehouseService
 gatehouse_service = GatehouseService()
 
-@router.post("/gatehouse/create", response_model=GateHouse, tags=["gatehouse"])
-async def create_gatehouse(gatehouse_create: GateHouse = Body(...)) -> GateHouse:
-    return gatehouse_service.create_gatehouse(gatehouse_create)
-
-@router.get("/gatehouse/get/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
-async def get_gatehouse(gatehouse_document_ID: str) -> GateHouse:
-    result = gatehouse_service.get_gatehouse(gatehouse_document_ID)
-    if not result:
-        raise HTTPException(status_code=404, detail="gatehouse not found.")
-    return result
+router = APIRouter()
+connected_clients: Dict[str, WebSocket] = {}  # Sử dụng dict để lưu trữ các client kết nối
 
 @router.put("/gatehouse/update/{gatehouse_document_ID}", response_model=GateHouse, tags=["gatehouse"])
-async def update_gatehouse(gatehouse_document_ID: str, gatehouse_update: GateHouse = Body(...)) -> GateHouse:
+async def update_gatehouse(websocket: WebSocket, gatehouse_document_ID: str, gatehouse_update: GateHouse = Body(...)) -> GateHouse:
     result = gatehouse_service.get_gatehouse(gatehouse_document_ID)
     if not result:
         raise HTTPException(status_code=404, detail="gatehouse not found.")
+
+    # Lưu trữ client WebSocket theo địa chỉ IP
+    connected_clients[websocket.client.host] = websocket
+
+    # Gửi dữ liệu cập nhật cho client app Android có địa chỉ IP cụ thể
+    target_ip = "192.168.0.1"  # Địa chỉ IP cần gửi dữ liệu
+    if target_ip in connected_clients:
+        target_client = connected_clients[target_ip]
+        await target_client.send_json(gatehouse_update.dict())
+
     return gatehouse_service.update_gatehouse(gatehouse_document_ID, gatehouse_update)
--- routers/test_ws.py
+++ routers/test_ws.py
@@ -3,27 +3,26 @@ from fastapi.websockets import WebSocketDisconnect
 import asyncio
 from services.sensors_service import SensorsService 
 from schemas.sensors_schema import Sensors
-
 router = APIRouter()
-connected_clients = set()
+connected_clients = {}  # Sử dụng dict để lưu trữ các client kết nối
+
 sensors_service = SensorsService()
 
-@router.put("/ws", response_model=Sensors, tags=["test"])
+@router.put("/ws/test2", response_model=Sensors, tags=["test"])
 async def websocket_endpoint(websocket: WebSocket, sensors_json: Sensors = Body(...)):
     await websocket.accept()
-    connected_clients.add(websocket)
-    
+    connected_clients[websocket.client.host] = websocket  # Lưu trữ client theo địa chỉ IP
     try:
         while True:
             data = await websocket.receive_text()
             # Xử lý dữ liệu nhận được từ nhà thông minh
             print("data", data)
-            print("sensors_json", sensors_json)
             # sensors_service.update_sensors(sensors_document_ID, sensors_update)
-            # Ví dụ: led_service.update_led(data)
             
-            # Gửi dữ liệu cập nhật cho tất cả các client đang kết nối
-            for client in connected_clients:
-                await client.send_text(data)
+            # Gửi dữ liệu cập nhật cho client có địa chỉ IP cụ thể
+            target_ip = "http://127.0.0.1:5000/"  # Địa chỉ IP cần gửi dữ liệu
+            if target_ip in connected_clients:
+                target_client = connected_clients[target_ip]
+                await target_client.send_text(data)
     except WebSocketDisconnect:
-        connected_clients.remove(websocket)
+        del connected_clients[websocket.client.host]  # Xóa client khỏi danh sách khi ngắt kết nối
